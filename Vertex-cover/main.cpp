4a5cd939465a8f04cc7401c1e5f19a2ca979f65c
Hyungmin Ji <miniji@umich.edu>
2023-04-30 13:47:50 +0900

    Initial Commit

---
+++ Vertex-cover/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Vertex-cover
+//
+//  Created by Hyung Min Ji on 4/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
